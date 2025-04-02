1b7779fbb7a63959b1f0c9f2d7bb1ef65b1d5b7a
Jorge Lara <jelch2002@gmail.com>
2025-04-02 11:37:36 -0600

    nope

--- bitcoin/src/p2p/mod.rs
+++ bitcoin/src/p2p/mod.rs
@@ -207,13 +207,13 @@ impl Magic {
     /// Bitcoin mainnet network magic bytes.
     /// 
     /// 0xd2, 0xb9, 0xba, 0xf9
-    pub const BITCOIN: Self = Self([0xd2, 0xb9, 0xba, 0xf9]);
+    pub const BITCOIN: Self = Self([0xF9, 0xBA, 0xB9, 0xD2]);
     /// Bitcoin testnet network magic bytes.
-    pub const TESTNET: Self = Self([0xd2, 0xb9, 0xba, 0xf9]);
+    pub const TESTNET: Self = Self([0xF9, 0xBA, 0xB9, 0xD2]);
     /// Bitcoin signet network magic bytes.
-    pub const SIGNET: Self = Self([0xd2, 0xb9, 0xba, 0xf9]);
+    pub const SIGNET: Self = Self([0xF9, 0xBA, 0xB9, 0xD2]);
     /// Bitcoin regtest network magic bytes.
-    pub const REGTEST: Self = Self([0xd2, 0xb9, 0xba, 0xf9]);
+    pub const REGTEST: Self = Self([0xF9, 0xBA, 0xB9, 0xD2]);
 
     /// Create network magic from bytes.
     pub fn from_bytes(bytes: [u8; 4]) -> Magic { Magic(bytes) }
